94cd059a1879177c0f865f6d7a6d188066cecbbd
Rahul gogra <rgogra914@gmail.com>
2024-11-29 16:57:15 +0530

    completed build changes

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
         rollupOptions: {
-            external: ["axios"], // Mark 'axios' as an external dependency
-        },
-    },
-    resolve: {
-        alias: {
-            axios: "axios", // Explicitly alias the axios package
+            external: [],
         },
     },
 });
